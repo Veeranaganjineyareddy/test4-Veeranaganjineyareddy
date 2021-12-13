c7909c247412a72687097f50438673bfba3bec65
Veeranaganjiney <94883268+Veeranaganjineyareddy@users.noreply.github.com>
2021-12-13 13:39:12 +0000

    adding 4 functions

--- p2original.c
+++ p2original.c
@@ -1,27 +1,29 @@
-45#include <stdio.h>
+#include<stdio.h>
 int input()
 {
   int a;
-  printf("enter any number");
-  scanf("%d" , &a);
-  return a;
+  printf(" Enter value\n");
+  scanf("%d",&a);
+ return a;
 }
 int add(int a,int b)
 {
-  int sum ;
-  sum=a+b;
-  return sum;
+  int c;
+  c=a+b;
+  return c;
+
 }
 int output(int sum)
 {
-  printf("sum of a&b is %d",sum);
+  printf( "Sum is %d",sum);
 }
 int main()
 {
   int a,b,sum;
   a=input();
   b=input();
-  sum=add (a,b);
-  output (sum);
+  sum=add(a,b);
+  output(sum);
   return 0;
+
 }
