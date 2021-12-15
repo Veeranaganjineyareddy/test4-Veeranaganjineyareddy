a06e67874c1a694224a2da24457dffdddb702594
Veeranaganjiney <94883268+Veeranaganjineyareddy@users.noreply.github.com>
2021-12-15 09:47:39 +0000

    4 functions using pass by value and pass by reference

---
+++ p3final.c
@@ -0,0 +1,31 @@
+gbd#include <stdio.h>
+int input();
+int add(int a, int b, int *sum);1
+void output(int a,int b,int sum);
+
+int main()
+{
+  int rslt,a,b,res;
+  a=input();
+  b = input();  
+  res = add(a,b, &rslt);
+  output(a,b,res);
+  return 0;
+}
+int input()
+{
+  int x;
+  printf("enter the value\n");
+  scanf("%d",&x);
+  return x;
+}
+
+int add(int a, int b, int *sum)
+{
+  *sum=a+b;
+  return *sum;
+}
+void output(int a,int b,int sum)
+{
+  printf("The answer = %d", sum);
+}
