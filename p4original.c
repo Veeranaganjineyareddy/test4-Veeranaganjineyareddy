0dd8b3e0114dc08610208b90f4023865595c27b1
Veeranaganjiney <94883268+Veeranaganjineyareddy@users.noreply.github.com>
2021-12-29 08:22:42 +0000

    pass by value

---
+++ p4original.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int input()
+{
+int s;
+printf("enter the value:");
+scanf("%d",&s);
+return s;
+}
+int cmp(int a, int b, int c)
+{
+   if((a>b)&&(a>c))
+   {
+     return a;
+   }
+   else if((b>c)&&(b>a))
+   {
+     return b;
+   }
+    else
+   {
+   return c;
+   }
+}
+
+  void output(int a, int b,int c,int s)
+{
+  printf("among %d,%d,%d\n%d is greatest",a,b,c,s);
+
+}
+int main()
+{
+  int r,p,t,s;
+  r=input();
+  t=input();
+  p=input();
+  s=cmp(r,t,p);
+  output(r,t,p,s);
+  return 0;
+}
